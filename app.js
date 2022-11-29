ada9325b8e0ba1c74bcaa25bcc197762619b68e3
Priyanshu Maurya <mohitknkush@gmail.com>
2022-11-29 23:41:46 +0530

    authentication v-2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -16,10 +17,14 @@ app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
+
 
 const User = new mongoose.model("User", userSchema)
 
