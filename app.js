c153411ec27eb8976721cf3fad9222c196882d4c
Priyanshu Maurya <mohitknkush@gmail.com>
2022-11-30 00:01:19 +0530

    environmental variable and gitignore

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +7,8 @@ const mongoose = require("mongoose");
 const encrypt = require('mongoose-encryption');
 
 const app = express();
+
+const secret = process.env.SECRET;
 
 app.use(bodyparser.urlencoded({
   extended: true
@@ -22,9 +25,9 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
 
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema)
 
